55d6b5d43c1a61a25394b127a70219a99c65094e
Ren-shi <brandonlesch112@gmail.com>
2023-10-30 12:52:21 +0200

    Movie Frontend set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './component/search/search.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
